017690d3ca48e357e58bf8c7b9f569203ffff601
akoeth-portfolio <dukewellington61@googlemail.com>
2020-11-11 20:47:32 +0100

    640px layout

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -56,6 +56,10 @@ export const HeroContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (min-width: 600px) {
+    top: 4rem;
+  }
 `;
 
 export const HeroH1 = styled.h1`
